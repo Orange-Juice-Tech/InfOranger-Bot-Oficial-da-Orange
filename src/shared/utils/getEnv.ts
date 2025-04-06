e798850a857bd77ed799ed37c77285e9ddc87eb2
parlandin <56051040+parlandin@users.noreply.github.com>
2025-04-06 20:57:05 -0300

    fix: permitir valor padrão para variáveis ​​de ambiente ausentes na função getEnv

--- src/shared/utils/getEnv.ts
+++ src/shared/utils/getEnv.ts
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const getEnv = (key: string) => {
+const getEnv = (key: string, default_value?: string) => {
   const value = process.env[key];
+
   if (!value) {
-    throw new Error(`Environment variable ${key} is not set`);
+    if (default_value) {
+      return default_value;
+    }
+
+    throw new Error(`Missing environment variable: ${key}`);
   }
 
   return value;
