f986257894496e5a89a7218e93dcf777f43f455a
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-12 17:42:56 -0300

    chore: adicionando auto-start para pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       script: "npm",
       args: "start",
       instances: 1,
-      autorestart: false,
+      autorestart: true,
       watch: false,
       max_memory_restart: "500M",
     },
