9d5629aa72a2f417d4bf5d500b136bc4a1fe8740
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-14 18:09:57 -0300

    feat: adiciona job StarDay para envio de mensagens semanais de divulgação

---
+++ src/shared/constants/starMessages.ts
@@ -0,0 +1,29 @@
+const developMessage = `# DIA DA ESTRELINHA ⭐️
+
+## Chegou a hora de dar um up naquele seu repositório favorito!
+
+Mandem seus próprios projetos / repositórios que vocês queiram divulgar para que os outros membros mandem aquela estrela marota.
+
+## :sparkles: **A regra é clara Arnaldo:** Mandou seu repositório, tem que curtir o dos outros coleguinhas tbm hein!
+
+
+|| @everyone @Membro  @here||
+## BORA COMEÇAR! :arrow_down:`;
+
+const designMessage = `# DIA DA ESTRELINHA - UX E DESIGN ⭐️
+
+## Chegou a hora de dar um up naquele seu projeto favorito!
+
+Mandem seus próprios projetos / portfólios que vocês queiram divulgar para que os outros membros deixem aquele like.
+
+## :sparkles: **A regra é clara Arnaldo:** Mandou seu portfolio, tem que curtir o dos outros coleguinhas tbm hein!
+
+@UX | User Experience Designer @UI | User Interface Designer @Designer 
+|| @everyone @Membro  @here ||
+## BORA COMEÇAR! :arrow_down:`;
+
+export const starMessages = {
+  develop: developMessage,
+  design: designMessage,
+};
+export type StarMessagesType = keyof typeof starMessages;
