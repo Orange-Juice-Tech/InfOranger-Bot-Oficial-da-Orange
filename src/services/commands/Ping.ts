50ca8734dbce3acbb8dabc6e2888eed24953c403
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-14 18:37:55 -0300

    feat: implementa comando ping com respostas aleatórias e adiciona constantes de respostas

--- src/services/commands/Ping.ts
+++ src/services/commands/Ping.ts
@@ -1,4 +1,5 @@
 import { CacheType, CommandInteraction } from "discord.js";
+import { pingResponses } from "@constants/pingResponses";
 
 class PingCommand {
   public async execute(
@@ -6,8 +7,25 @@ class PingCommand {
   ): Promise<undefined> {
     if (!interaction.isCommand()) return;
 
+    const category = Object.keys(pingResponses) as Array<
+      keyof typeof pingResponses
+    >;
+    const randomCategory =
+      category[Math.floor(Math.random() * category.length)];
+
+    if (!randomCategory) {
+      await interaction.reply({
+        content: "Estou fazendo um cafezinho, aguarde um momento!",
+      });
+      return;
+    }
+
+    const phrase = pingResponses[randomCategory];
+
+    const randomResponse = phrase[Math.floor(Math.random() * phrase.length)];
+
     await interaction.reply({
-      content: "Estou fazendo um cafezinho, aguarde...",
+      content: randomResponse,
     });
   }
 }
