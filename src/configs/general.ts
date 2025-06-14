6b3b722b50022a96b2db7ab8c834743c88a40f6d
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-14 15:28:57 -0300

    fix: atualizando o nome do bot e nomenclatura dos canais

--- src/configs/general.ts
+++ src/configs/general.ts
@@ -1,7 +1,7 @@
 import getEnv from "@shared/utils/getEnv";
 
 export default {
-  botName: getEnv("BOT_NAME", "Orange Juice"),
+  botName: getEnv("BOT_NAME", "InfOranger, A Estagiária"),
   guildId: getEnv("DISCORD_GUILD_ID", "847518545156112424"),
   guildName: getEnv("DISCORD_GUILD_NAME=", "Orange Juice"),
 };
