777ccb768357335e08b0f1f243acc186f360ee75
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-24 16:44:35 -0300

    feat: adiciona serviço para escutar mensagens de hackthon e responder interações

--- src/events/chat/messageCreate.ts
+++ src/events/chat/messageCreate.ts
@@ -1,5 +1,6 @@
 import { Event } from "@interfaces/events";
 import { listenFormationMessageService } from "@services/events/listenFormationMessage";
+import { listenHackthonMessageService } from "@services/events/listenHackthonMessage";
 import { reactProjectsWithStarService } from "@services/events/reactProjectsWithStar";
 
 export default new Event({
@@ -9,5 +10,6 @@ export default new Event({
 
     await reactProjectsWithStarService.listen(interaction);
     await listenFormationMessageService.listen(interaction);
+    await listenHackthonMessageService.listen(interaction);
   },
 });
